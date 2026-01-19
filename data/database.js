bd5a1a8b15f669eee2bdcbd72c8515ce3e019a43
adityakrawat <aditya.rawat@hotmail.com>
2026-01-19 12:20:57 +0530

    Initial commit

---
+++ data/database.js
@@ -0,0 +1,26 @@
+import { MongoClient} from 'mongodb';
+
+const connectionProtocol = process.env.MONGODB_CONNECTION_PROTOCOL
+const clusterAddress = process.env.MONGODB_CLUSTER_ADDRESS;
+const dbUser = process.env.MONGODB_USERNAME;
+const dbPassword = process.env.MONGODB_PASSWORD;
+const dbName = process.env.MONGODB_DB_NAME;
+
+const uri = `${connectionProtocol}://${dbUser}:${dbPassword}@${clusterAddress}/${dbName}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri);
+
+console.log('trying to connect to database');
+
+try {
+    await client.connect();
+    await client.db(dbName).command({ ping: 1 });
+    console.log('Connected successfully to database');
+} catch (e) {
+    console.log('Connection failed.');
+    await client.close();
+    console.log('Connection closed.');
+}
+
+const database = client.db(dbName);
+
+export default database;
